66a38965590d773fa87c3f945046bdb574247a8a
Wim Vandamme <wim.vandamme@inabox.be>
2020-10-18 21:16:25 +0200

    menu

---
+++ Assets/Scripts/CameraCollider.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollider : MonoBehaviour
+{
+
+    public Canvas canvas;
+
+    private void OnEnable()
+    {
+        canvas.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (canvas.isActiveAndEnabled)
+        {
+            canvas.gameObject.transform.rotation = Quaternion.LookRotation(canvas.worldCamera.transform.forward);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider_)
+    {
+        if (collider_.gameObject.tag == "MainCamera")
+        {
+            canvas.gameObject.SetActive(true);
+            canvas.worldCamera = collider_.GetComponent<Camera>();
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider collider_)
+    {
+        if (collider_.gameObject.tag == "MainCamera")
+        {
+            canvas.gameObject.SetActive(false);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Rotating.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotating : MonoBehaviour
+{
+
+    public float speed = 30.0f;
+
+    bool rotate = false;
+
+
+    public void OnToggle()
+    {
+        rotate = !rotate;
+    }
+
+    void Update()
+    {
+        if (rotate)
+        {
+            transform.Rotate(Vector3.one * speed * Time.deltaTime);
+        }   
+    }
+}
