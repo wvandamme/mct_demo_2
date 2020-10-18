9e9d504fd2c46dd7b682fd9fb5f7a5f6e8b9a21b
Wim Vandamme <wim.vandamme@inabox.be>
2020-10-18 22:11:29 +0200

    ongoing dev

--- Assets/Scripts/TouchContoller.cs
+++ Assets/Scripts/TouchContoller.cs
@@ -1,14 +1,30 @@
-﻿using System.Collections;
+﻿
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 public class TouchContoller : MonoBehaviour
 {
+
+    public ARRaycastManager raycast_manager;
+    public Camera camera;
+    public GameObject world;
+
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
     void Update()
     {
-        if (Input.touchCount > 0)
-        {
-            Debug.Log("TOUCH");
+
+        if (Input.touchCount != 1) return;
+        
+        Touch touch = Input.GetTouch(0);
+
+        if (touch.phase != TouchPhase.Ended) return;
+
+        if (raycast_manager.Raycast(touch.position, hits))
+        {  
+            Instantiate(world, hits[0].pose.position, Quaternion.identity);
         }
+          
     }
 }
