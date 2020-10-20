9ca0898c8094a6c7939b440ca97eb8aecacb244f
wvandamme <wim.vandamme@inabox.be>
2020-10-20 12:55:49 +0200

    raycasts

--- Assets/Scripts/TouchContoller.cs
+++ Assets/Scripts/TouchContoller.cs
@@ -1,6 +1,7 @@
 ﻿
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.XR.ARFoundation;
 
 public class TouchContoller : MonoBehaviour
@@ -12,7 +13,7 @@ public class TouchContoller : MonoBehaviour
 
     private List<ARRaycastHit> hits = new List<ARRaycastHit>();
 
-    void Update()
+    void FixedUpdate()
     {
 
         if (Input.touchCount != 1) return;
@@ -20,6 +21,18 @@ public class TouchContoller : MonoBehaviour
         Touch touch = Input.GetTouch(0);
 
         if (touch.phase != TouchPhase.Ended) return;
+
+        if (EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+        {
+            return;
+        }
+
+        Ray ray = camera.ScreenPointToRay(touch.position);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log("HIT");
+        }
 
         if (raycast_manager.Raycast(touch.position, hits))
         {  
